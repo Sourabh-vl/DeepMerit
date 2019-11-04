3e7081306ade02fd6b931f009792ce5d2dbba513
Sourabh-vl <56428242+Sourabh-vl@users.noreply.github.com>
2019-11-04 18:15:02 +0530

    first code
    defining funcions and passing a dummy variable

---
+++ deepmerit.py
@@ -0,0 +1,27 @@
+class DeepMerit:
+    
+    def counterfeitDrug
+      def captureImage(data):
+            return data
+    
+      def tagImage(data):
+            return data
+    
+      def shapleyImage(data):
+          print("hi")
+          return data
+    
+      def cpcaImage(data):
+          return data
+    
+      def mabImage(data):
+          return data
+      
+      def mlModel(data):
+          return data
+       
+      def notify(data):
+          return data
+      
+      return
+    
