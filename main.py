35d4542735f029768d098aeb4fc9f959da68dac7
Sourabh-vl <56428242+Sourabh-vl@users.noreply.github.com>
2019-11-05 11:17:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,2 @@
 from deepmerit import DeepMerit
-print(DeepMerit.counterfeitDrug.shapleyImage("This is the shapely image function inside Counterfeit Drug"))
+print(DeepMerit.counerfietdrug.shapleyImage("This is the shapely image function inside Counterfeit Drug"))
