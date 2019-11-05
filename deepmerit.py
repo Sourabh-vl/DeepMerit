203203484a0f65e03a062c37960fe4f630f592f8
Sourabh-vl <56428242+Sourabh-vl@users.noreply.github.com>
2019-11-05 17:18:22 +0530

    Update deepmerit.py

--- deepmerit.py
+++ deepmerit.py
@@ -1,25 +1,5 @@
 class DeepMerit:
-   def counerfietdrug(data):   
+   def counterfietdrug(data):
+       print("hi")
+       return data
       
-      def captureImage(data):
-            return data
-    
-      def tagImage(data):
-            return data
-    
-      def shapleyImage(data):
-            print("hi")
-            return data
-    
-      def cpcaImage(data):
-            return data
-    
-      def mabImage(data):
-            return data
-      
-      def mlModel(data):
-            return data
-       
-      def notify(data):
-            return data
-    
